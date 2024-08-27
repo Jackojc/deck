2df1b9971ed967ebbc4629c47292959cabef08d8
Jacob Sinclair <jcbsnclr@outlook.com>
2024-08-27 17:14:47 +0100

    define ident, symbol, and integer tokens

--- cdc/src/cdc.c
+++ cdc/src/cdc.c
@@ -48,10 +48,13 @@ typedef enum {
     TOK_INT,
     TOK_STR,
     TOK_IDENT,
+    TOK_SYMBOL,
 
     TOK_KW_INT,
     TOK_KW_DEF,
     TOK_KW_LET,
+
+    TOK_NONE,
 } token_kind_t;
 
 typedef struct {
@@ -66,9 +69,11 @@ const char *tok_to_str[] = {[TOK_WS] = "WS",
                             [TOK_INT] = "INT",
                             [TOK_STR] = "STR",
                             [TOK_IDENT] = "IDENT",
+                            [TOK_SYMBOL] = "SYMBOL",
                             [TOK_KW_INT] = "KW_INT",
                             [TOK_KW_DEF] = "KW_DEF",
-                            [TOK_KW_LET] = "KW_LET"};
+                            [TOK_KW_LET] = "KW_LET",
+                            [TOK_NONE] = "NONE"};
 
 void lexer_init(lexer_t *lx, const char *src) {
     lx->src = src;
@@ -93,6 +98,17 @@ static bool take_if(lexer_t *lx, ccond_t cond) {
     return cond(c) && take(lx);
 }
 
+static bool take_str(lexer_t *lx, const char *str) {
+    size_t len = strlen(str);
+
+    if (strncmp(lx->src + lx->ptr, str, len) == 0) {
+        lx->ptr += len;
+        return true;
+    }
+
+    return false;
+}
+
 static bool take_while(lexer_t *lx, ccond_t cond) {
     bool taken = false;
     while (take_if(lx, cond)) taken = true;
@@ -100,13 +116,13 @@ static bool take_while(lexer_t *lx, ccond_t cond) {
 }
 
 static bool kw_cmp(const char *src, size_t len, const char *kw) {
-    return len == strlen(kw) && strncmp(src, kw, len);
+    return len == strlen(kw) && strncmp(src, kw, len) == 0;
 }
 
-// static bool take_if_eq(lexer_t *lx, char exp) {
-//     char c = peek(lx);
-//     return c == exp && take(lx);
-// }
+static bool take_if_eq(lexer_t *lx, char exp) {
+    char c = peek(lx);
+    return c == exp && take(lx);
+}
 
 /*
 lexer:
@@ -151,9 +167,9 @@ static bool is_ws(char c) {
     return isspace(c);
 }
 
-// static bool is_not_nl(char c) {
-//     return c != '\n';
-// }
+static bool is_not_nl(char c) {
+    return c != '\n';
+}
 
 static bool is_ident_start(char c) {
     return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
@@ -171,12 +187,14 @@ static bool is_ident_body(char c) {
  * producers
  */
 
-static void produce_ident(lexer_t *lx, token_t *tok) {
-    tok->kind = TOK_IDENT;
+static bool produce_ident(lexer_t *lx, token_t *tok) {
     tok->start = lx->ptr;
+
+    if (!take_if(lx, is_ident_start)) return false;
 
     take_while(lx, is_ident_body);
 
+    tok->kind = TOK_IDENT;
     tok->end = lx->ptr;
 
     size_t len = tok->end - tok->start;
@@ -188,36 +206,91 @@ static void produce_ident(lexer_t *lx, token_t *tok) {
         tok->kind = TOK_KW_INT;
     else if (kw_cmp(src, len, "def"))
         tok->kind = TOK_KW_DEF;
+
+    return true;
 }
 
-static void produce_while(lexer_t *lx, token_t *tok, ccond_t cond, token_kind_t kind) {
+static bool produce_symbol(lexer_t *lx, token_t *tok) {
+    tok->start = lx->ptr;
+
+    if (!take_if_eq(lx, '#')) return false;
+
+    take_while(lx, is_ident_body);
+
+    tok->kind = TOK_SYMBOL;
+    tok->end = lx->ptr;
+
+    size_t len = tok->end - tok->start;
+    const char *src = lx->src + tok->start;
+
+    if (kw_cmp(src, len, "let"))
+        tok->kind = TOK_KW_LET;
+    else if (kw_cmp(src, len, "int"))
+        tok->kind = TOK_KW_INT;
+    else if (kw_cmp(src, len, "def"))
+        tok->kind = TOK_KW_DEF;
+
+    return true;
+}
+
+static bool produce_while(lexer_t *lx, token_t *tok, ccond_t cond, token_kind_t kind) {
     tok->kind = kind;
     tok->start = lx->ptr;
 
-    take_while(lx, cond);
+    bool res = take_while(lx, cond);
 
     tok->end = lx->ptr;
+
+    return res;
 }
 
 bool lexer_next(lexer_t *lx, token_t *tok) {
-    char c = peek(lx);
-    if (!c) return false;
+    tok->start = lx->ptr;
+    tok->end = 0;
+    tok->kind = TOK_NONE;
 
-    if (is_ident_start(c))
-        produce_ident(lx, tok);
-    else if (is_ws(c))
-        produce_while(lx, tok, is_ws, TOK_WS);
+    char c = peek(lx);
+
+    if (!c) {
+        tok->kind = TOK_EOF;
+        tok->start = lx->ptr;
+        tok->end = lx->ptr + 1;
+        return true;
+    }
+
+    while (take_while(lx, is_ws) || take_str(lx, "#!"))
+        if (take_str(lx, "#!")) take_while(lx, is_not_nl);
+
+    if (produce_ident(lx, tok))
+        printf("lexer: ident\n");
+    else if (produce_symbol(lx, tok))
+        printf("lexer: symbol\n");
+    else if (produce_while(lx, tok, is_digit, TOK_INT))
+        printf("lexer: int\n");
+    else
+        return false;
 
     return true;
 }
 
 int main(void) {
-    const char *src = "let def int truelol";
+    const char *src = "     #! foo bar baz\nint let def foobar 123 #abc";
     lexer_t lx;
     lexer_init(&lx, src);
 
     token_t tok;
-    while (lexer_next(&lx, &tok)) printf("tok: %s\n", tok_to_str[tok.kind]);
+    while (lexer_next(&lx, &tok) && tok.kind != TOK_EOF)
+        printf("tok: %s : %d\n", tok_to_str[tok.kind], (int)lx.src[tok.start]);
+
+    size_t len = 3;
+    char *s = "def";
+
+    if (kw_cmp(s, len, "let"))
+        printf("let\n");
+    else if (kw_cmp(s, len, "int"))
+        printf("int\n");
+    else if (kw_cmp(s, len, "def"))
+        printf("def");
 
     return 0;
 }
