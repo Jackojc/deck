5098672c45094bd63d5e772b3bf96123affd61a6
Jack Clarke <jackojc@gmail.com>
2024-08-31 18:39:40 +0100

    Update makefile and add man page placeholder

---
+++ cdc/config.mk
@@ -0,0 +1,23 @@
+# cdc version
+VERSION = 2024-08-30
+
+# prefixes
+PREFIX = /usr/local
+MANPREFIX = $(PREFIX)/share/man
+
+PKG_CONFIG = pkg-config
+
+# headers/libs
+INCS = -Iinclude/
+LIBS =
+DEPS =
+
+# flags
+CFLAGS += --std=c11 -Wall -Wextra -O3 -g
+
+CFLAGS += $(foreach dep, $(DEPS), $(shell pkg-config --cflags $(dep)))
+LDFLAGS += $(foreach dep, $(DEPS), $(shell pkg-config --libs $(dep)))
+
+DECK_CPPFLAGS = -DVERSION=\"$(VERSION)\"
+DECK_CFLAGS = $(INCS) $(DECK_CPPFLAGS) $(CPPFLAGS) $(CFLAGS)
+DECK_LDFLAGS = $(LIBS) $(LDFLAGS)
